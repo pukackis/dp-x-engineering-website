794ccd6c5db8b95fcf300bc53276b9f2c434737e
Szymon <104310438+pukackis@users.noreply.github.com>
2025-07-02 10:54:58 +0200

    Fix Tailwind CSS integration by manually configuring PostCSS and Tailwind CSS v3.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react()],
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de'],
